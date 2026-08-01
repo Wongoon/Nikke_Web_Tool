6d952718f4caaf2bb8a99187f926aa1fe5a4324f
Wongoon <93926767+Wongoon@users.noreply.github.com>
2026-08-01 19:28:18 +0900

    Update: 'api-test.js'

--- api-test.js
+++ api-test.js
@@ -1,1 +1,20 @@
+const fetch = require("node-fetch");
 
+async function test(){
+    const res = await fetch("API URL",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                intl_open_id:"YOUR_ID",
+                nikke_area_id:83
+            })
+        }
+    );
+
+    console.log(await res.text());
+}
+
+test();
